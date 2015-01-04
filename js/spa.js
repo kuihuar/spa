3139651480897bcc3f4e28944bdcc5f9c7746121
kuihuar <kuihuar@yahoo.com>
2015-01-04 23:45:26 +0800

    commit
    commit

--- js/spa.js
+++ js/spa.js
@@ -1,1 +1,21 @@
-spa.js
+/**
+* spa.js
+* Root namespace module
+*/
+
+/* jslint settings 
+	brower:true, continue:true,devel:true,indent:2,maxerr:50,
+	regexp:true, nomen:true, plusplus:true, newcap:true, sloppy:true,
+	vars:true, while:true
+*/
+/** global $, spa */
+var spa=(function(){
+	var initModule=function($container){
+		$container.html(
+			'<h1 style="display:inline-block;margin:25px;">'
+			+'hello world!'
+			+'</h1>'
+			);
+	}
+	return {initModule:initModule};
+}());
