e9a499578cd5c2cc5797ef95841dea97f591b695
kuihuar <kuihuar@yahoo.com>
2015-01-11 21:04:43 +0800

    chat.js
    chat.js  setSliderPsition method done

--- js/spa.chat.js
+++ js/spa.chat.js
@@ -16,32 +16,160 @@ spa.chat = (function(){
 	var
 	configMap = {
 		main_html: String()
-		+ '<div style="padding:1em; color:#fff">'
-			+ 'Hello chat'
-		+ '</div>',
-		settable_map:{}
+		+'<div class="spa-chat">'
+		  +'<div class="spa-chat-head">'
+		    +'<div class="spa-chat-head-toggle">+</div>'
+		    +'<div class="spa-chat-head-title">'
+		      +'chat'
+		    +'</div>'
+		  +'</div>'
+		  +'<div class="spa-chat-closer">x</div>'
+		  +'<div class="spa-chat-sizer">'
+		    +'<div class="spa-chat-msgs"></div>'
+		    +'<div class="spa-chat-box">'
+		    	+'<input type="text" />'
+		    	+'<div>send</div>'
+		    +'</div>'
+		  +'</div>'
+		+'</div>',
+		settable_map:{//chat 的所有设置
+			slider_open_time:true,
+			slider_close_time:true,
+			slider_opened_em:true,
+			slider_closed_em:true,
+			slider_opened_title:true,
+			slider_closed_title:true,
+
+			chat_model:true,
+			poeple_model:true,
+			set_chat_anchor:true,
+		},
+		slider_open_time:250,
+		slider_close_time:250,
+		slider_ipened_em:16,
+		slider_closed_em:2,
+		slider_opened_title:'Click to close',
+		slider_closed_title:'Click to open'
 	},
-	stateMap = {$container: null},
+
+	stateMap = {
+		$append_target:null,
+		position_type:'closed',
+		px_per_em:0,
+		slider_hidden_px:0,
+		slider_closed_px:0,
+		slider_opened_px:0
+
+	},
 	jqueryMap = {},
-	setJqueryMap, configModule, initModule
+	setJqueryMap, getEmSize, setPxSizes, setSliderPosition,
+	onClickToggle, configModule, initModule
 	;
 
 	//---------------------END MODULE SCOPE VARIABLES--------------------------
 
 	//-----------------------BEGIN UTILITY METHODS-----------------------------
+	getEmSize(elem){
+		return Number(
+			getComputedStyle(elem, '').fontSize.match(/\d*\.?\d*/)[0];
+		);
+	};
 	//------------------------END UTILITY METHODS------------------------------
 
 	//------------------------BEGIN DOM METHODS-------------------------------- 
 	// Begin DOM method /setJqueryMap/
 	setJqueryMap = function(){
-		var $container = stateMap.$container;
-		jqueryMap = { $container: $container };
+		var
+		$append_target = stateMap.$append_target,
+		$slider=$append_target.find('.spa-chat');
+		jqueryMap={
+			$slider:$slider,
+			$head:$slider.find('.spa-chat-head');
+			$toggle:$slider.find('.spa-chat-toggle');
+			$title:$slider.find('.spa-chat-title');
+			$sizer:$slider.find('spa-chat-sizer');
+			$msgs:$slider.find('spa-chat-msgs');
+			$box:$slider.find('.spa-chat-box');
+			$input:$slider.find('.sap-chat-input input[type=text]');
+		};
 	};
 	// End DOM method /setJqueryMap/
-
+	// Begin DOM method /setPxSizes/
+	setPxSizes = function(){
+		var px_er_em, opened_height_em;
+		px_per_em = getEmSize(jqueryMap.$slider.get(0));
+		opened_height_em = configMap.slider_opened_em;
+		stateMap.px_per_em = px_per_em;
+		stateMap.slier_closed_px = configMap.slider_closed_em * px_per_em;
+		stateMap.slider_opened_px = opened_height_em * px_per_em;  
+		jqueryMap.$sizer.css({
+			height:(opened_height_em - 2) * px_per_em
+		});
+	}
+	// End DOM method /setPxSizes/
 	// Begin Public DOM method /setSliderPosition/
-	
-	// End Public DOM method /setSliderPostion/
+	// Example: spa.chat.setSliderPosition('closed');
+	// Purpose: Ensure chat slider is in the requested state
+	// Arguments: 
+	//    * position_type - enum('closed', 'opened', or 'hidder')
+	//    * callback - optional callback at end of animation.
+	//      (callback receives slider DOM element as argument)
+	// Action: 
+	//    Leaves slider in current state if it matches requested,
+	//    otherwise animate to requested state
+	// Reteurns:
+	//  * true - requested state achieved
+	//  * false  - requested state not achieved
+	// Throws: none
+	//
+	setSliderPosition = function(position_type, callback){
+		var
+		height_px, animate_time, slider_title, toggle_text;
+
+		//return true if slider already in requested position
+		if(stateMap.position_type === position_type){
+			return true;
+		}
+		//prepare animate parameters
+		switch(position_type){
+			case 'opened':
+				height_px = stateMap.slider_opened_px;
+				animate_time = configMap.slider_open_time;
+				slider_title = configMap.slider_opened_title;
+				toggle_text = '=';
+			break;
+			case 'hidden':
+				height_px = 0;
+				animate_time = configMap.slider_open_time;
+				slider_title = '';
+				toggle_text = '+';
+			break;
+			case 'closed':
+				height_px = configMap.slider_closed_px;
+				animate_time = configMap.slider_close_time;
+				slider_title = configMap.slider_closed_tiele;
+				toggle_text = '+';
+			break;
+			default:
+			//bail for unknown position_type
+			return false;
+		}
+		//animate slider position change
+		stateMap.$slider_type = '';
+		jqueryMap.$slider.animate({
+			{height: height_px},
+			animate_time,
+			function(){
+				jqueryMap.$toogle.prop('title', slider_title);
+				jqueryMap.$goggle.text(toggle_text);
+				stateMap.position_type = position_type;
+				if(callback){
+					callback(jqueryMap.$slider);
+				}
+			}
+		});
+	};
+	// End Public DOM method /setSliderPosition/
 
 	//------------------------END DOM METHODS----------------------------------
 
